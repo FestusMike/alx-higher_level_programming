4fbb90b3f0110a147199812c8de62ddc23b256e5
FestusMike <festusmike98@gmail.com>
2023-08-26 15:06:58 +0100

    Massive Corrections

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 """
-Displays all cities of a given state from the
-states table of the database hbtn_0e_4_usa.
-Safe from SQL injections.
-Usage: ./5-filter_cities.py <mysql username> \
-                            <mysql password> \
-                            <database name> \
-                            <state name searched>
+5-filter_cities: safely select cities of states matching argument
 """
-import sys
+
+
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cs = db.cursor()
-    cs.execute("SELECT * FROM `cities` as `c` \
-                INNER JOIN `states` as `s` \
-                   ON `c`.`state_id` = `s`.`id` \
-                ORDER BY `c`.`id`")
-    print(", ".join([ct[2] for ct in cs.fetchall() if ct[4] == sys.argv[4]]))
+    db = MySQLdb.connect(
+      host="localhost",
+      user=sys.argv[1],
+      passwd=sys.argv[2],
+      database=sys.argv[3],
+      port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM cities RIGHT JOIN states ON cities.state_id =\
+        states.id WHERE states.name = '{}'".format(sys.argv[4]))
+    results = cur.fetchall()
+    print(", ".join([result[2] for result in results]))
+    cur.close()
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """
-Defines a State model.
-Inherits from SQLAlchemy Base and links to the MySQL table states.
+model_state: contains class definition of State
 """
-
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -11,12 +9,7 @@ Base = declarative_base()
 
 
 class State(Base):
-    """Represents a state for a MySQL database.
-
-    __tablename__ (str): The name of the MySQL table to store States.
-    id (sqlalchemy.Integer): The state's id.
-    name (sqlalchemy.String): The state's name.
-    """
-    __tablename__ = "states"
-    id = Column(Integer, primary_key=True)
+    """base class state"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
